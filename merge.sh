2159d379a6dd2c6485c09f1f6820779843136ecd
agofton <a.gofton@murdoch.edu.au>
2018-02-26 15:38:38 +0800

    Update merge.sh

--- merge.sh
+++ merge.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-# Require usearch10 to be installed on $PATH or else mod script to specift complete path to usearch10
+# Require "usearch10" to be installed on $PATH or else mod script to include complete path to usearch10
 #
 # Usage
 #
-# $ merge.sh [raw data dir] [output dir] [min alignment overlap: Int] [min merged length: Int]
+# $ ./merge.sh [raw data dir] [output dir] [min alignment overlap: Int] [min merged length: Int]
 #
 ################################################################################
 ################################################################################
@@ -15,26 +15,35 @@ min_overlap=${3?Error: min overlap not given}
 min_len=${4?Error: min merged length no given}
 
 mkdir ${merged_data_dir}
-mkdir working1
+mkdir phiX-filter-tmp
+
+# 1. Merge paired end sequences
+
 for file in ${raw_data_dir}/*R1_001.fastq
   do
 
-usearch10 -fastq_mergepairs ${file} \
--reverse "${raw_data_dir}/$(basename -s R1_001.fastq ${file})R2_001.fastq" \
--fastqout "working1/$(basename -s L001_R1_001.fastq "$file")merged.fastq" \
--fastq_minovlen ${min_overlap} \
--fastq_minmergelen ${min_len} \
--relabel @
+    echo --------------------
+    echo merging sample: $file
+    echo --------------------
 
-  done
+    usearch10 -fastq_mergepairs ${file} \
+              -reverse "${raw_data_dir}/$(basename -s R1_001.fastq ${file})R2_001.fastq" \
+              -fastqout "phiX-filter-tmp/$(basename -s L001_R1_001.fastq "$file")merged.fastq" \
+              -fastq_minovlen ${min_overlap} \
+              -fastq_minmergelen ${min_len} \
+              -relabel @
+done
 
-# Filter PhiX
+# 2. Filter out any phiX sequences
 
-for file in working1/*.fastq
+for file in phiX-filter-tmp/*.fastq
   do
-    usearch10 -filter_phix ${file} -output "${merged_data_dir}/$(basename ${file})"
-  done
 
-rm -r working1
+    usearch10 -filter_phix ${file} \
+              -output "${merged_data_dir}/$(basename ${file})"
+
+done
+
+
 ################################################################################
 ################################################################################
